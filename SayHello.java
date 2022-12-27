c18fc8579a5117f560ab39d3ae7b09a7cde6d6ca
indah-ayu <indah.ayu@gmail.com>
2022-12-27 13:46:50 +0700

    First Commit From Branch Satu

--- SayHello.java
+++ SayHello.java
@@ -2,6 +2,7 @@ public class SayHello
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("This is a new line");
 		System.out.println("Hello there!");
 		System.out.println("Hi there!");
 		System.out.println("Hi hi there!");
